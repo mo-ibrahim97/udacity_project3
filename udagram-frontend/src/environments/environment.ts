4a19208c46bf5a4f39245f3dbe304c448b5e1618
mo-ebrahim <mohamedebrahim979777@gmail.com>
2022-12-30 02:57:23 +0200

    CI/CD done

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0/'
+  apiHost: 'http:// a1969ef9868ef4d9fb9cb8e0646e895b-830899586.us-east-1.elb.amazonaws.com:8080/api/v0/'
 };
 
 /*
